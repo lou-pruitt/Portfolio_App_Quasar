57624564495daeee606edf2493e6c6f336b559f4
Lou Pruitt <Lpruitt2004@gmail.com>
2019-04-27 19:11:39 -0700

    Lazy load components, add skills component

---
+++ src/components/Skills.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="q-pa-md col-12 col-md-10 offset-md-1">
+    <q-card flat bordered class="skills-card text-center">
+      <q-card-section>
+        <div class="text-h6">Skills / Tools</div>
+      </q-card-section>
+
+      <q-separator color="blue" inset/>
+
+      <q-card-section class="row">
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-html5-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-css3-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-javascript-plain colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-jquery-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-react-original-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-vuejs-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-php-plain colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-mysql-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-git-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-github-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-bootstrap-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-nodejs-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-amazonwebservices-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-ubuntu-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-slack-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-visualstudio-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-phpstorm-plain-wordmark colored"></i>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-4 mt-4">
+          <i class="devicon-gimp-plain colored"></i>
+        </div>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<style scoped>
+  .skills-card {
+    font-size: 5rem;
+  }
+
+  @media (min-width: 768px) {
+    .skills-card {
+    font-size: 6rem;
+    }
+  }
+</style>
+
+<script>
+export default {}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,7 @@
   <q-page class="row">
     <app-header></app-header>
     <app-about></app-about>
+    <app-skills></app-skills>
   </q-page>
 </template>
 
@@ -9,14 +10,16 @@
 </style>
 
 <script>
-import appAbout from 'components/About.vue'
-import appHeader from 'components/Header.vue'
+// import appAbout from 'components/About.vue'
+// import appHeader from 'components/Header.vue'
+// import appSkills from 'components/Skills.vue'
 
 export default {
   name: 'PageIndex',
   components: {
-    appAbout,
-    appHeader
+    appAbout: () => import('components/About.vue'),
+    appHeader: () => import('components/Header.vue'),
+    appSkills: () => import('components/Skills.vue')
   }
 }
 </script>
